6bedd5450bb7babb55ba07d042cac15fe6bd8560
Drew Ross <mail.drew.ross@gmail.com>
2021-04-13 13:37:27 -0700

    add testUserPoints to modData

--- data/modData.js
+++ data/modData.js
@@ -6,13 +6,20 @@
 const testUserSubmissions = (SubmissionID) => {
   return {
     writing: [
-      {URL: 'https://picsum.photos/id/1/400', PageNum: 1, SubmissionID},
-      {URL: 'https://picsum.photos/id/101/400', PageNum: 2, SubmissionID}
+      { URL: 'https://picsum.photos/id/1/400', PageNum: 1, SubmissionID },
+      { URL: 'https://picsum.photos/id/101/400', PageNum: 2, SubmissionID }
     ],
-    drawing: {URL: 'https://picsum.photos/id/201/400', SubmissionID}
+    drawing: {URL: 'https://picsum.photos/id/201/400', SubmissionID }
   }
 }
 
+const testUserPoints = [
+  { WritingPoints: 50, DrawingPoints: 15, MemberID: 1, SubmissionID: 1 },
+  { WritingPoints: 25, DrawingPoints: 10, MemberID: 1, SubmissionID: 2 },
+];
+
+
 module.exports = {
-  testUserSubmissions
+  testUserSubmissions,
+  testUserPoints
 }
